c8557873062b22d236d69a2e81fffe65d6b77c27
Dandi Rizky <dandirizkyy94@gmail.com>
2023-06-17 16:03:37 +0700

    fix navigation to regsiter text in login form

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -108,7 +108,7 @@ export const Login = () => {
           Login
         </button>
         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-          Already have an account ?{" "}
+          Don't have an account ?{" "}
           <Link
             to="/register"
             className="font-medium text-primary-600 hover:underline dark:text-primary-500"
